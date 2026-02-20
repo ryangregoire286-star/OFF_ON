af2cab5d17b573457419930c35fab441645cfcd3
BinaryConvertz <realcoder2023@gmail.com>
2026-02-20 11:28:20 -0500

    added args as command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::io::stdin;
 use std::sync::atomic::{AtomicBool, Ordering};
 
@@ -23,38 +24,72 @@ enum Type {
     On()
 }
 
-fn add(x: i32) -> i32 {
-    x + 1
-}
-
-fn sub(x: i32) -> i32 {
-    x - 1
-}
 
 fn main() {
 
+
     unsafe {
         while IS_WRITEABLE == true {
-            let mut str = String::new();
+            for arg in env::args() {
+                if arg.trim().contains("1") {
+                    let mut str = String::new();
 
-            println!("{}", "Enter 1 || 0: ");
-            stdin().read_line(&mut str).expect("No Entered Input");
+                    println!("{}", "Enter 1 || 0: ");
+                    stdin().read_line(&mut str).expect("No Entered Input");
 
-            let num_str: i32 = str.trim().parse().unwrap();
+                    let num_str: i32 = str.trim().parse().unwrap();
 
-            static X: AtomicBool = AtomicBool::new(false);
-            static Y: AtomicBool = AtomicBool::new(true);
+                    static X: AtomicBool = AtomicBool::new(false);
 
-            if num_str == sub(1) {
-                let off = Type::Off();
+                    if num_str == 1 {
+                        let off = Type::Off();
 
-                check_light_bulb(String::from("False"), X.load(Ordering::Relaxed));
-                println!("{:?}", off);
-            } else if num_str == add(0){
-                let on = Type::On();
-                check_light_bulb(String::from("True"), Y.load(Ordering::Relaxed));
-                println!("{:?}", on);
+                        check_light_bulb(String::from("False"), X.load(Ordering::Relaxed));
+                        println!("{:?}", off);
+                    }
+                } else if arg.trim().contains("0") {
+                    let mut str = String::new();
+
+                    println!("{}", "Enter 1 || 0: ");
+                    stdin().read_line(&mut str).expect("No Entered Input");
+
+                    let num_str: i32 = str.trim().parse().unwrap();
+
+                    static Y: AtomicBool = AtomicBool::new(true);
+
+                    if num_str == 0 {
+                        let on = Type::On();
+                        check_light_bulb(String::from("True"), Y.load(Ordering::Relaxed));
+                        println!("{:?}", on);
+                    }
+                } else {
+                    let mut str = String::new();
+
+                    println!("{}", "Enter 1 || 0: ");
+                    stdin().read_line(&mut str).expect("No Entered Input");
+
+                    let num_str: i32 = str.trim().parse().unwrap();
+
+                    static Y: AtomicBool = AtomicBool::new(true);
+
+                    if num_str == 0 {
+                        let on = Type::On();
+                        check_light_bulb(String::from("True"), Y.load(Ordering::Relaxed));
+                        println!("{:?}", on);
+                    }
+
+                    if num_str == 1 {
+                        let off = Type::Off();
+
+
+                        static X: AtomicBool = AtomicBool::new(false);
+
+                        check_light_bulb(String::from("False"), X.load(Ordering::Relaxed));
+                        println!("{:?}", off);
+                    }
+                }
             }
+
 
             IS_WRITEABLE = false;
         }
